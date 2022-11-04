6c5aba7f8507542414c39040c18d1ec8d3533f37
veronikakinasih <3103120226@student.smktelkom-pwt.sch.id>
2022-11-04 11:32:37 +0700

    Authentication

--- ebook/app/Http/Controllers/AuthController.php
+++ ebook/app/Http/Controllers/AuthController.php
@@ -3,15 +3,77 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function me(){
         return [
-            'Nis' => 3103120226,
-            'Name' => 'Veronika Arisetyo Kinasih',
-            'Phone' => '0895385082005',
+            'NIS' => 3103120221,
+            'Name' => 'Tifany Fadilah Ahnaf',
+            'Phone' => '085702092095',
             'Class' => 'XII RPL 7'
+        ];
+    }
+
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string|max:100',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed|min:8'
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password'])
+        ]);
+
+        $token = $user->createToken('tokensaya')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        //check email
+        $user = User::where('email', $fields['email'])->first();
+
+        //check password
+        if (!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => 'unauthorized'
+            ], 401);
+        }
+
+        $token = $user->createToken('tokensaya')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return [
+            'message' => 'Logged out'
         ];
     }
 }
--- ebook/routes/api.php
+++ ebook/routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\AuthorController;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -21,18 +22,26 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/me', [AuthController::class, 'me']);
 
-// Route::get('/books', [BookController::class, 'index']);
-// Route::get('/books/{id}', [BookController::class, 'show']);
-// Route::post('/books', [BookController::class, 'store']);
-// Route::put('/books/{id}', [BookController::class, 'update']);
-// Route::delete('/books/{id}', [BookController::class, 'destroy']);
+//public route
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::resource('books', BookController::class)->except(
-    ['create', 'edit']
-);
+Route::get('me', [AuthController::class, 'me']);
 
-Route::resource('authors', AuthorController::class)->except(
-    ['create', 'edit']
-);
+Route::get('author', [AuthController::class, 'author']);
+
+Route::resource('books', BookController::class)->except([
+    'create', 'edit'
+]);
+
+Route::resource('authors', AuthorController::class)->except([
+    'create', 'edit'
+]);
+
+//protected route
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('books', BookController::class)->except('create', 'edit', 'show', 'index');
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::resource('authors', AuthorController::class)->except('create', 'edit', 'show', 'index');
+});
